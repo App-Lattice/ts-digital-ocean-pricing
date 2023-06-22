0fa22bc9fc6f8af5dd2e01f12167b86cd83467c2
Angelo Manos <angelo@angelomanos.com>
2023-06-22 09:05:59 -0500

    add cluster to export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import volumes from './volumes';
 import loadBalancer from './loadBalancer';
 import droplet from './droplet/droplet';
+import cluster from 'cluster';
 
 export default {
   volumes,
   loadBalancer,
   droplet,
+  cluster,
 };
